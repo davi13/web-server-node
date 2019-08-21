7aeb0042a189675e5e1a4f8bcb4b66c85174d625
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-21 22:54:28 +0200

    app.js setting a template for html handler

--- src/app.js
+++ src/app.js
@@ -4,11 +4,35 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+app.set('views', viewsPath);
 
 
 app.set('view engine', 'hbs');
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Davi'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Davi'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Davi'
+    })
+
+})
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
