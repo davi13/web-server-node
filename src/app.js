ec8d088dae2c62eb74225451c8ee42603c279bef
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-21 09:02:16 +0200

    public: added hepl page and about page

--- src/app.js
+++ src/app.js
@@ -7,30 +7,6 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.send('Hello express')
-});
-
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'davi',
-        age: 38
-    })
-});
-app.get('/about', (req, res) => {
-    res.send('<h1>about</h1>')
-});
-app.get('/weather', (req, res) => {
-    res.send([
-        {
-            latitude: 37.8267,
-            logitude: -122.4233,
-            location: 'boston'
-
-        }
-    ])
-});
-
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
