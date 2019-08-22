209484f50e8b9035b10fb8b33d713870fe281cfa
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-22 08:51:39 +0200

    app.js: creating the partials for the app

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
 
 //Setup handlebars engine and views loacation
 app.set('views', viewsPath);
 app.set('view engine', 'hbs');
 
+//Setup static directory to server
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
