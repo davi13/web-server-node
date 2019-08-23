ca87c34acc1672d9ccfe59403950abeef3fac7de
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-23 10:32:25 +0200

    app.js:working with query

--- src/app.js
+++ src/app.js
@@ -37,6 +37,25 @@ app.get('/help', (req, res) => {
         name: 'Davi'
     });
 });
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'hot summer',
+        location: 'paris'
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+
+    }
+    console.log(req.query);
+    res.send({
+        products: []
+    });
+})
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
