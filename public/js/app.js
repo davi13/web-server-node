899cf93164138fb517796a28addbbf924a692467
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-24 18:29:50 +0200

    js->app.js sending the data to the front end  and using .textContent

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ console.log('client side javascript file loaded')
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -13,8 +15,8 @@ weatherForm.addEventListener('submit', (e) => {
                     if (data.error) {
                         console.log(data.error)
                     } else {
-                        console.log(data.location)
-                        console.log(data.forecast)
+                        messageOne.textContent = data.location;
+                        messageTwo.textContent = data.forecast;
                     }
                 });
         });
