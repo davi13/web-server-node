a44327b138245ca888f149c031983b705a27e949
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-24 17:45:24 +0200

    js->app.js: using fetch with the Api

--- src/app.js
+++ src/app.js
@@ -45,23 +45,22 @@ app.get('/weather', (req, res) => {
             error: 'You must provide a address term'
         })
     }
-    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
-            return res.send({ Error: error });
+            return res.send({ error });
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-                return res.send({ Error: error });
+                return res.send({ error });
             }
             // console.log(location);
             // console.log('Data: ', forecastData);
             res.send({
                 forecast: forecastData,
-                location: location,
+                location,
                 address: req.query.address
             });
-
         })
     })
 
