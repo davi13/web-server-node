a2933918375c3f9da1c65e5ec276ba306b4df209
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-24 18:04:53 +0200

    index.hbs:added a search form to the index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,13 @@
 console.log('client side javascript file loaded')
 fetch('http://localhost:3000/weather?address=marseille')
     .then((response) => {
-
         response.json()
             .then((data) => {
                 if (data.error) {
                     console.log(data.error)
                 } else {
-                    console.log(data)
+                    console.log(data.location)
+                    console.log(data.forecast)
                 }
             })
     });
