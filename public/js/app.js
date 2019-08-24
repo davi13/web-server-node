e5e913e2c2de35a02a2d9ea98ac97ee15f0a2d33
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-24 18:43:19 +0200

    js->app.js sending the data to the front end  and using .textContent

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,10 @@ const messageTwo = document.querySelector('#message-2');
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location)
         .then((response) => {
             response.json()
@@ -15,11 +19,13 @@ weatherForm.addEventListener('submit', (e) => {
                     if (data.error) {
                         console.log(data.error)
                     } else {
+                        search.value = '';
                         messageOne.textContent = data.location;
                         messageTwo.textContent = data.forecast;
                     }
                 });
         });
+
 });
 
 
