62a1655c350dc14c7438cd3a9f1bb100eff0c591
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-25 22:18:49 +0200

    added the lowes tempeture and the higher

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (latitude, logitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. the max temperature is ' + body.daily.data[0].temperatureHigh + ' and the low temperature is ' + body.daily.data[0].temperatureLow);
         }
     })
 }
