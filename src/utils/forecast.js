6485b5003555a4326062252eecc9aa16fa93942b
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-25 21:19:53 +0200

    removen useless code

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,19 +10,7 @@ const forecast = (latitude, logitude, callback) => {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.');
         }
     })
-
 }
 module.exports = forecast;
-// const url = 'https://api.darksky.net/forecast/62b6f3d89534c3cabdee2373423dbd76/37.8267,-122.4233?';
 
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log(`Unable to connect to weather service`);
-//     } else if (response.body.error) {
-//         console.log(`Unable to connect to weather service`);
-//     } else {
-//         console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} out there`);
-//         console.log(`There is a  ${response.body.currently.precipProbability}% chance of rain`);
-//     }
-// })
 
